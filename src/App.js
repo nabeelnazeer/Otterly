da0e211ed7c2da09fa3dcc40642f421152b2e0d2
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2024-03-15 17:21:13 +0530

    branch,coursestructure(need change)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import HorizontalBlock from './components/header/HorizontalBlock';
 import CourseSelection from './components/header/CourseSelection';
 import CopyrightComponent from './components/header/CopyrightComponent';
 import UtilityPage from './components/header/UtilityPage'; // Import the UtilityPage component
+import CourseStructurePage from './components/header/CourseStructurePage';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
 
           {/* Define the route for the UtilityPage component */}
           <Route path="/UtilityPage" element={<UtilityPage />} />
+          <Route path = "/CourseStructurePage" element={<CourseStructurePage/>} />
         </Routes>
       </div>
     </Router>
