4e61e264be540b17f6a4385cb560600f06167421
nabeelnazeer <nabeel777@ug.cusat.ac.in>
2024-03-22 03:31:38 +0530

    uncaught error still persist

--- src/components/header/LearnWindow.js
+++ src/components/header/LearnWindow.js
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/theme-github';
-// import VirtualAssistant from './VirtualAssistant'; // Import the VirtualAssistant component
 import './LearnWindow.css';
 
-function LearnWindow() {
-  const [htmlCode, setHtmlCode] = useState('');
-  
-  // Function to handle changes in the code editor
-  const handleChange = (newCode) => {
-    setHtmlCode(newCode);
-    // You can add additional logic here if needed
-  };
+// Basic sanitization function to remove script tags
+function sanitizeHtml(html) {
+ return html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+}
 
-  return (
+function LearnWindow() {
+ const [htmlCode, setHtmlCode] = useState('');
+  
+ const handleChange = (newCode) => {
+    setHtmlCode(newCode);
+ };
+
+ return (
     <div className="learn-window">
       <h1 className="app-title">HTML Project: A Cat Photo App</h1>
       <div className="content-wrapper">
         <div className="code-editor">
-          {/* Render the VirtualAssistant component */}
-          {/* <VirtualAssistant htmlCode={htmlCode} setHtmlCode={setHtmlCode} /> */}
           <h2>Code Editor</h2>
           <AceEditor
             mode="html"
@@ -34,16 +34,16 @@ function LearnWindow() {
             fontSize={16}
             wrapEnabled
             showPrintMargin={false}
-            showGutter={true} // Enable line numbering
+            showGutter={true}
           />
         </div>
         <div className="output-display">
           <h2>Output Display</h2>
-          <div dangerouslySetInnerHTML={{ __html: htmlCode }} />
+          <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(htmlCode) }} />
         </div>
       </div>
     </div>
-  );
+ );
 }
 
 export default LearnWindow;
